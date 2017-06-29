8306278b373a7d2f05a069811db7ab02bead48eb
kittycat2002 <josh.masters.02@gmail.com>
2017-06-29 16:03:39 -0400

    Added Queue System

--- music.lua
+++ music.lua
@@ -5,6 +5,7 @@ local timer = require('timer')
 local client = discordia.Client()
 local config,_,err = json.parse(fs.readFileSync("music.config"))
 musiclist = {}
+queue = {}
 local function musiclistfunction(dir)
   for i=1,#fs.readdirSync(dir) do
 	if fs.statSync(dir.."/"..fs.readdirSync(dir)[i]).type == 'directory' then
@@ -22,21 +23,50 @@ client:on('ready', function()
   while true do
     if channel.memberCount > 1 then
       musiclistfunction(config.musicdir)
-	  if fs.readFileSync("nextsong.txt") ~= '{"name":"","dir":""}' then
-	    music = json.parse(fs.readFileSync('nextsong.txt'))
-		fs.writeFileSync('nextsong.txt','{"name":"","dir":""}')
-		music.dir = config.musicdir.."/"..music.dir
+	  if #queue > 0 then
+	    music = queue[1]
+		table.remove(queue,1)
 	  else
 		music = musiclist[math.random(1,#musiclist)]
 	  end
-	  musicname = string.sub(music.name,string.find(music.name,'^.*%.'))
-	  musicname = string.sub(musicname,1,-2)
-	  musicname = string.gsub(musicname,"[%-_]"," ")
+	  musicname = string.gsub(string.sub(string.sub(music.name,string.find(music.name,'^.*%.')),1,-2),"[%-_]"," ")
 	  client:setGameName(musicname)
       connection:playFile(music.dir.."/"..music.name)
 	  client:setGameName()
 	end
     timer.sleep(1000)
+  end
+end)
+
+client:on('messageCreate', function(message)
+  if message.member.roleCount > 0 then
+    if string.lower(string.sub(message.content,1,7)) == "!queue " then
+      musiclistfunction(config.musicdir)
+      for i=1,#musiclist do
+	    musicq = musiclist[i]
+	    if string.lower(string.gsub(string.sub(string.sub(musicq.name,string.find(musicq.name,'^.*%.')),1,-2),"[%-_]"," ")) == string.lower(string.gsub(string.sub(message.content,8),"[%-_]"," ")) then
+	      table.insert(queue,musicq)
+		  break
+	    end
+      end
+    elseif string.lower(string.sub(message.content,1,11)) == "!clearqueue" then
+	  queue = {}
+	elseif string.lower(string.sub(message.content,1,13)) == "!queueremove " then
+	  if tonumber(string.sub(message.content,14)) then
+	    table.remove(queue,tonumber(string.sub(message.content,14)))
+	  end
+	end
+  end
+  if string.lower(string.sub(message.content,1,10)) == "!queuelist" then
+    if #queue == 0 then
+	  message.channel:sendMessage("The queue is currently empty.")
+	else
+	  local queuelist = ""
+      for i=1,#queue do
+	    queuelist = queuelist..i..": "..string.gsub(string.sub(string.sub(queue[i].name,string.find(queue[i].name,'^.*%.')),1,-2),"[%-_]"," ").."\n"
+	  end
+	  message.channel:sendMessage(queuelist)
+	end
   end
 end)
 
